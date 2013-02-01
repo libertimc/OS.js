8a7e734b7f6bb67c0fef1cc61ba57d15a1fbb475
Anders Evenrud <andersevenrud@gmail.com>
2013-02-01 19:25:55 +0100

    Node.js: Moved API from main.js to api.js

---
+++ src/api.js
@@ -0,0 +1,492 @@
+/*!
+ * @file
+ * OS.js - JavaScript Operating System - API
+ *
+ * Copyright (c) 2011-2012, Anders Evenrud <andersevenrud@gmail.com>
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met: 
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer. 
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution. 
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @author  Anders Evenrud <andersevenrud@gmail.com>
+ * @licence Simplified BSD License
+ * @created 2013-01-27
+ */
+"use strict";
+
+///////////////////////////////////////////////////////////////////////////////
+// IMPORTS
+///////////////////////////////////////////////////////////////////////////////
+
+// Internal
+var _config    = require('../config.js'),
+    _registry  = require(_config.PATH_SRC + '/registry.js'),
+    _settings  = require(_config.PATH_SRC + '/settings.js'),
+    _preload   = require(_config.PATH_SRC + '/preload.js'),
+    _packages  = require(_config.PATH_SRC + '/packages.js'),
+    _vfs       = require(_config.PATH_SRC + '/vfs.js'),
+    _user      = require(_config.PATH_SRC + '/user.js'),
+    _services  = require(_config.PATH_SRC + '/services.js'),
+    _locale    = require(_config.PATH_SRC + '/locale.js'),
+    _session   = require(_config.PATH_SRC + '/session.js');
+
+// External
+var express = require('express'),
+    sprintf = require('sprintf').sprintf,
+    swig    = require('swig'),
+    syslog  = require('node-syslog');
+
+///////////////////////////////////////////////////////////////////////////////
+// CLASSES
+///////////////////////////////////////////////////////////////////////////////
+
+
+function request(req, res) {
+  console.log('POST /');
+
+  var jsn, action, response = null;
+  try {
+    jsn     = req.body;//.objectData;
+    action  = jsn.action;
+
+    console.log('AJAX /', action);
+  } catch ( e ) {
+    jsn = {};
+    console.error(e);
+  }
+
+  if ( action === null  ) {
+    defaultJSONResponse(req, res);
+  } else {
+    var logged_in = false;
+    if ( (req.session && req.session.user) && (typeof req.session.user == 'object') ) {
+      logged_in = req.session.user;
+    }
+
+    var _respond = function(http_code, http_data) {
+      // FIXME: Check for 'error' and pull out message if it's an object
+      res.json(http_code, http_data);
+    };
+
+    // First check if we need a user
+    var need_auth = [
+      "snapshotList", "snapshotLoad", "snapshotSave", "snapshotDelete", "updateCache",
+      "init", "settings", "logout", "shutdown","user", "event", "package", "service", "call"
+    ];
+
+    var i = 0, l = need_auth.length;
+    for ( i; i < l; i++ ) {
+      if ( (need_auth[i] == action) && (logged_in === false) ) {
+        _respond(200,  {
+            success : false,
+            error   : 'You are not logged in!',
+            result  : null
+        });
+        return;
+        //break;
+      }
+    }
+
+    // Call API
+    switch ( action ) {
+      case 'boot' :
+        var restore = false;
+
+        if ( logged_in !== false ) {
+          if ( req.session.user.lock ) {
+            restore = true;
+          }
+        }
+
+        syslog.log(syslog.LOG_INFO, "starting up a new client");
+
+        response = {
+          success : true,
+          result  : {
+            environment : {
+              bugreporting : _config.BUGREPORT_ENABLE,
+              production   : _config.ENV_PRODUCTION,
+              demo         : _config.ENV_DEMO,
+              cache        : _config.ENABLE_CACHE,
+              connection   : _config.ENV_PLATFORM,
+              ssl          : _config.ENV_SSL,
+              autologin    : _config.AUTOLOGIN_ENABLE,
+              restored     : restore,
+              hosts        : {
+                frontend      : _config.HOST_FRONTEND,
+                'static'      : _config.HOST_STATIC
+              }
+            }
+          }
+        };
+
+        _respond(200,  response);
+      break;
+
+      /*case 'logout' :
+      break;*/
+
+      case 'login' :
+        var username = jsn.form ? (jsn.form.username || "") : "";
+        var password = jsn.form ? (jsn.form.password || "") : "";
+        var resume   = (jsn.resume === true || jsn.resume === "true");
+
+        var _success = function(user, packages, resume_registry, resume_session) {
+          user.sid = req.sessionID;
+          res.cookie('osjs_sessionid', req.sessionID);
+
+          response = {
+            user          : user,
+            registry      : {
+              revision      : _config.SETTINGS_REVISION,
+              settings      : _settings.getDefaultSettings(_registry.defaults),
+              packages      : packages,
+              preload       : _preload.getPreloadFiles()
+            },
+            restore      : {
+              registry      : resume_registry,
+              session       : resume_session
+            },
+            locale       : {
+              system        : _config.DEFAULT_LANGUAGE,
+              browser       : user.language
+            }
+          };
+
+
+          _respond(200, {success: true, result: response});
+
+          req.session.user      = user;
+
+          syslog.log(syslog.LOG_INFO, "client[" + req.session.user.username + "] logged in...");
+        };
+
+        var _failure = function(msg) {
+          syslog.log(syslog.LOG_ERROR, "Login failed: " + msg);
+
+          console.error('login::_failure()', msg);
+
+          req.session.user = null;
+          res.cookie('osjs_sessionid', null);
+
+          _respond(200, {success: false, error: msg, result: null});
+        };
+
+        var _proceed = function(puser) {
+          _packages.getInstalledPackages(puser, function(success, result) {
+            if ( success ) {
+              _user.resume(puser, function(resume_registry, resume_session) {
+                _success(puser, result, resume_registry, resume_session);
+              });
+            } else {
+              _failure(result);
+            }
+          });
+        };
+
+        if ( resume ) {
+          _proceed(req.session.user);
+        } else {
+          if ( _config.AUTOLOGIN_ENABLE ) {
+            username = _config.AUTOLOGIN_USERNAME;
+            password = _config.AUTOLOGIN_PASSWORD;
+          }
+
+          _user.login(username, password, function(success, data) {
+            if ( success ) {
+              _proceed(data);
+            } else {
+              _failure(data || "Failed to log in!");
+            }
+          });
+        }
+      break;
+
+      case 'shutdown' :
+        var registry = jsn.registry || {};
+        var session  = jsn.session || {};
+        var save     = (jsn.save === true || jsn.save === "true");
+        var duration = jsn.duration;
+
+        var __done = function() {
+          _respond(200, {success: true, result: true});
+
+          try {
+            syslog.log(syslog.LOG_INFO, "client[" + req.session.user.username + "] shutdown complete");
+          } catch ( err )  {
+            syslog.log(syslog.LOG_INFO, "client[???] shutdown complete");
+          }
+
+          req.session.user = null;
+          req.session.destroy();
+
+        };
+
+        if ( (req.session && req.session.user) && (typeof req.session.user == 'object') ) {
+          req.session.user.lock = false;
+          _user.logout(req.session.user, registry, session, save, duration, function() {
+            __done();
+          });
+        } else {
+          __done();
+        }
+
+      break;
+
+      case 'updateCache' :
+        _packages.getInstalledPackages(req.session.user, function(success, result) {
+          if ( success ) {
+            _respond(200, {success: true, result: {
+              packages : result
+            }});
+          } else {
+            _respond(200, {success: false, error: result, result: null});
+          }
+        });
+      break;
+
+      case 'settings' :
+        syslog.log(syslog.LOG_INFO, "saving client[" + req.session.user.username + "] settings");
+
+        _user.store(req.session.user, jsn.registry, null, function(err) {
+          _respond(200, {success: err ? false : true, result: err ? err : true});
+        });
+      break;
+
+      case 'user' :
+        var uid = parseInt(jsn.uid, 10) || 0;
+        switch ( jsn.method ) {
+          case 'list' :
+            // TODO
+            break;
+
+          case 'update':
+            // TODO
+            break;
+
+          case 'delete':
+            // TODO
+            break;
+
+          case 'info' :
+          default     :
+            _respond(200, {success: true, result: req.session.user.info});
+            return;
+            break;
+        }
+
+        defaultJSONResponse(req, res);
+      break;
+
+      case 'event' : // TODO
+        var ev_instance = jsn.instance || null;
+        var ev_action   = ev_instance.action || null;
+        var ev_args     = ev_instance.args || [];
+        var ev_name     = ev_instance.name ? ev_instance.name.replace(/[^A-z0-9]/, '') : null;
+
+        var user = req.session.user;
+
+        if ( ev_action === null || ev_instance === null || ev_name === null ) {
+          _respond(200, { success: false, error: "Invalid event!", result: null });
+        } else {
+          _packages.getInstalledSystemPackages(user.language, function(success, result) {
+            if ( success ) {
+              var load_class = false;
+
+              for ( var pn in result ) {
+                if ( result.hasOwnProperty(pn) ) {
+                  if ( pn == ev_name ) {
+                    load_class = ev_name + ".node.js";
+                    break;
+                  }
+                }
+              }
+
+              if ( load_class === false ) {
+                _respond(200, { success: false, error: 'Cannot handle this event!', result: null });
+              } else {
+                var _cpath = ([_config.PATH_PACKAGES, load_class]).join("/");
+                var _class = null;
+                try {
+                  _class = require(_cpath);
+                } catch ( err ) {
+                  console.error('event', err);
+                  _respond(200, { success: false, error: err.message, result: null });
+                  return;
+                }
+
+                if ( _class !== null ) {
+                  try {
+                    _class.Event(ev_action, ev_args, function(esuccess, eresult) {
+                      if ( esuccess ) {
+                        _respond(200, { success: true, result: eresult });
+                      } else {
+                        _respond(200, { success: false, error: eresult, result: null });
+                      }
+                    });
+                  } catch ( err ) {
+                    console.error('event', err);
+                    _respond(200, { success: false, error: err.message, result: null });
+                  }
+                }
+              }
+            } else {
+              _respond(200, { success: false, error: result, result: null });
+            }
+          });
+        }
+      break;
+
+      case 'package' :
+        if ( (jsn.operation) ) {
+          var failed = false;
+          switch ( jsn.operation ) {
+            case 'install' :
+              _packages.installPackage(req.session.user, jsn['archive'], function(success, result) {
+                if ( success ) {
+                  _respond(200, { success: true, result: result });
+                } else {
+                  _respond(200, { success: false, error: result, result: null });
+                }
+              });
+            break;
+
+            case 'uninstall' :
+              _packages.uninstallPackage(req.session.user, jsn['package'], function(success, result) {
+                if ( success ) {
+                  _respond(200, { success: true, result: result });
+                } else {
+                  _respond(200, { success: false, error: result, result: null });
+                }
+              });
+            break;
+
+            default :
+              failed = true;
+            break;
+          }
+        }
+
+        if ( failed ) {
+          _respond(200, { success: false, error: 'Invalid package operation!', result: null });
+        }
+      break;
+
+      case 'call' :
+        console.log("API::call()", jsn);
+        if ( (jsn.method && jsn.args) ) {
+          try {
+            var ok = _vfs.call(req.session.user, jsn.method, (jsn.args || []), function(vfssuccess, vfsresult) {
+              if ( vfssuccess ) {
+                _respond(200, { success: true, result: vfsresult });
+              } else {
+                _respond(200, { success: false, error: vfsresult, result: null });
+              }
+            });
+
+            if ( !ok ) {
+              _respond(200, { success: false, error: 'Invalid VFS action!', result: null });
+            }
+          } catch ( err ) {
+            console.error('call', err);
+            _respond(200, { success: false, error: err.message, result: null });
+          }
+        } else {
+          _respond(200, { success: false, error: 'Invalid VFS arguments!', result: null });
+        }
+        return;
+      break;
+
+      case 'service' : // TODO
+        defaultJSONResponse(req, res);
+      break;
+
+      case 'bug' : // TODO
+        defaultJSONResponse(req, res);
+      break;
+
+      case 'snapshotList'   :
+        _session.snapshotList(logged_in, function(success, result) {
+          if ( success ) {
+            _respond(200, {success: true, result: result});
+          } else {
+            _respond(200, {success: false, result: null, error: result});
+          }
+        });
+      break;
+
+      case 'snapshotSave'   :
+        if ( jsn.session && jsn.session.name && jsn.session.data ) {
+          _session.snapshotSave(logged_in, jsn.session.name, jsn.session.data, function(success, result) {
+            if ( success ) {
+              _respond(200, {success: true, result: result});
+            } else {
+              _respond(200, {success: false, result: null, error: result});
+            }
+          });
+        } else {
+          _respond(200, {success: false, result: null, error: 'Invalid snapshot save operation!'});
+        }
+      break;
+
+      case 'snapshotLoad'   :
+        if ( jsn.session && jsn.session.name ) {
+          _session.snapshotLoad(logged_in, jsn.session.name, function(success, result) {
+            if ( success ) {
+              _respond(200, {success: true, result: result});
+            } else {
+              _respond(200, {success: false, result: null, error: result});
+            }
+          });
+        } else {
+          _respond(200, {success: false, result: null, error: 'Invalid snapshot load operation!'});
+        }
+      break;
+
+      case 'snapshotDelete' :
+        if ( jsn.session && jsn.session.name ) {
+          _session.snapshotDelete(logged_in, jsn.session.name, function(success, result) {
+            if ( success ) {
+              _respond(200, {success: true, result: result});
+            } else {
+              _respond(200, {success: false, result: null, error: result});
+            }
+          });
+        } else {
+          _respond(200, {success: false, result: null, error: 'Invalid snapshot delete operation!'});
+        }
+      break;
+
+      default :
+        _respond(200, { success: false, error: 'Invalid action!', result: null });
+      break;
+    }
+  }
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// EXPORTS
+///////////////////////////////////////////////////////////////////////////////
+
+module.exports = {
+  request : request
+};
+
+
