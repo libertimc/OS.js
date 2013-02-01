a50c4877cce7bb0d94165a93fe3ca8e9d20e31ea
Anders Evenrud <andersevenrud@gmail.com>
2013-02-01 13:40:27 +0100

    Node.js: Archive now support zip list, extract and create

--- src/archive.js
+++ src/archive.js
@@ -35,7 +35,9 @@
 // IMPORTS
 ///////////////////////////////////////////////////////////////////////////////
 
-var spawn = require('child_process').spawn;
+var spawn = require('child_process').spawn,
+    exec  = require('child_process').exec,
+    fs    = require('fs');
 
 ///////////////////////////////////////////////////////////////////////////////
 // CLASSES
@@ -46,31 +48,108 @@ var Zip = function(filename) {
 };
 
 Zip.prototype = {
-  create : function(path) {
-    var zdata = new Buffer();
+  create : function(path, callback) {
+    var destination = this.filename;
+    var zdata = [];
+    var zlen = 0;
+    var errors = [];
 
     var zip = spawn('zip', ['-rj', '-', path]);
-    zip.stdout.on('data', function (data) {
-      zdata.write(data);
+
+    zip.stdout.on('chunk', function (chunk) {
+      zchunk.push(chunk);
+      zlen += chunk.length;
     });
 
     zip.stderr.on('data', function (data) {
       console.log('zip stderr: ' + data);
+      errors.push(data);
     });
 
     zip.on('exit', function (code) {
       if ( code !== 0 ) {
-        console.log('zip process exited with code ' + code);
+        callback(false, 'zip process exited with code ' + code, errors);
+      } else {
+        var buf = new Buffer(zlen);
+        for (var i=0, len = zdata.length, pos = 0; i < len; i++) {
+          zdata[i].copy(buf, pos);
+          pos += data[i].length;
+        }
+
+        fs.writeFile(destination, 'binary', buf.toString('binary'), function(err) {
+          if ( err ) {
+            callback(false, err, errors);
+          } else {
+            callback(true, true, errors);
+          }
+        });
       }
     });
   },
 
-  extact : function(dest, callback) {
-    callback(false, "TODO");
+  extract : function(destination, callback) {
+    var source = this.filename;
+    var errors = [];
+
+    var zip = spawn('unzip', [source, '-d', destination]);
+
+    zip.stdout.on('chunk', function (chunk) {
+    });
+
+    zip.stderr.on('data', function (data) {
+      console.log('unzip stderr: ' + data);
+      errors.push(data);
+    });
+
+    zip.on('exit', function (code) {
+      if ( code !== 0 ) {
+        callback(false, 'unzip process exited with code ' + code, errors);
+      } else {
+        callback(true, true);
+      }
+    });
   },
 
   ls : function(callback) {
-    callback(false, "TODO");
+    var source = this.filename;
+
+    exec('unzip -l ' + source, function(error, stdout, stderr) {
+      stdout = stdout || '';
+      stderr = stderr || '';
+
+      if ( error ) {
+        callback(false, error, stderr.split(/(\r?\n)/));
+      } else if ( !stdout ) {
+        callback(false, 'failed to run ?!', stderr.split(/(\r?\n)/));
+      } else {
+        var items = stdout.split(/(\r?\n)/), iter;
+        var list   = [];
+        var total  = 0;
+
+        if ( items.length > 4 ) {
+          for ( var i = 5; i < items.length; i++ ) {
+            iter = items[i].replace(/\s+/g, ' ').replace(/^\s*/g, '').replace(/\s*$/g, '');
+            iter = iter.split(' ');
+            if ( iter.length >= 4 ) {
+              list.push({
+                  'length' : parseInt(iter[0], 10),
+                  'date'   : iter[1],
+                  'time'   : iter[2],
+                  'name'   : iter[3]
+              });
+
+              total += parseInt(iter[0], 10);
+            }
+          }
+        }
+
+        callback(true, {
+            files   : list,
+            length  : list.length,
+            total   : total
+        });
+      }
+    });
   }
 };
 
@@ -83,6 +162,10 @@ module.exports = {
 
   ls : function(filename, callback) {
     (new Zip(filename)).ls(callback);
+  },
+
+  create : function(path, destination, callback) {
+    (new Zip(destination)).create(path, callback);
   },
 
   extract : function(filename, destination, callback) {
