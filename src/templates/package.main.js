7dcb964eb36d888af5a236891470b3cd295a8aef
Anders Evenrud <andersevenrud@gmail.com>
2013-02-23 22:29:53 +0100

    Node.js: More work on glade->js parser, Signals, enabled by default

--- src/templates/package.main.js
+++ src/templates/package.main.js
@@ -1,3 +1,4 @@
+
 /*!
  * {{ type }}: {{ package }}
  *
@@ -85,15 +86,90 @@ OSjs.Packages.{{ package|e }} = (function($, undefined) {
       destroy : function() {
         this._super();
       },
+{% for s in w.signals %}
+      /**
+       * Signal: [{{ s.id }}]::{{ s.signal }}()
+       */
+      {{ s.handler }} : function(el, ev) {
+        var self = this;
 
-{{ w.code_class }}
+  {%- if s.template == "DefaultFileOpen" %}
+        var my_callback = function(fname) {}; // FIXME
+        var cur         = (argv && argv['path'] ? argv['path'] : null);
 
+        this.app.defaultFileOpen(function(fname) {
+          my_callback(fname);
+        }, MIMES, null, cur);
+  {%- else if s.template == "DefaultFileSave" %}
+        var my_filename = (argv && argv['path'] ? argv['path'] : null);
+        var my_content  = ""; // FIXME
+        var my_callback = function(fname) {}; // FIXME
+
+        if ( my_filename ) {
+          this.app.defaultFileSave(my_filename, my_content, function(fname) {
+            my_callback(fname);
+          }, MIMES, undefined, false);
+        }
+  {%- else if s.template == "DefaultFileSaveAs" %}
+        var my_filename = (argv && argv['path'] ? argv['path'] : null);
+        var my_content  = ""; // FIXME
+        var my_callback = function(fname, fmime) {}; // FIXME
+
+        this.app.defaultFileSave(my_filename, my_content, function(fname) {
+          my_callback(fname);
+        }, MIMES, undefined, true);
+  {%- else if s.template == "DefaultClose" %}
+        this.$element.find(".ActionClose").click();
+  {%- else if s.template == "DefaultClipboardCopy" %}
+        this.app._clipboard("copy");
+  {%- else if s.template == "DefaultClipboardPaste" %}
+        this.app._clipboard("paste");
+  {%- else if s.template == "DefaultClipboardCut" %}
+        this.app._clipboard("cut");
+  {%- else if s.template == "DefaultClipboardDelete" %}
+        this.app._clipboard("delete");
+  {%- else if s.template == "DefaultClipboardSelectAll" %}
+        this.app._clipboard("select");
+  {%- else %}
+    {%- if s.signal == "file-set" %}
+        var my_path     = this.$element.find(".{{ s.id }} input[type=text]").val();
+        this.app.createFileDialog(function(fname) {
+          self.$element.find(".{{ s.id }} input[type=text]").val(fname);
+          self.$element.find(".{{ s.id }} input}type=hidden]").val(fname);
+        }, MIMES, "open", dirname(my_path));
+    {%- else if s.signal == "input-activate" %}
+        // do nothing
+    {%- else %}
+        // TODO
+    {%- endif %}
+  {%- endif %}
+      },
+{% endfor %}
       create : function(id, mcallback) {
         var el = this._super(id, mcallback);
         var self = this;
 
         if ( el ) {
-{{ w.code_create }}
+{% for s in w.signals %}
+          // Signal: [{{ s.id }}]::{{ s.signal }}()
+  {%- if s.signal == "file-set" %}
+          el.find(".{{ s.id }} button").click(function(ev) {
+            self.{{ s.handler }}(this, ev);
+          });
+  {%- else if s.signal == "input-activate" %}
+          el.find(".{{ s.id }}").keypress(function(ev) {
+            var k = ev.keyCode || ev.which;
+            if ( k == 13 ) {
+              self.{{ s.handler }}(this, ev);
+            }
+          });
+  {%- else %}
+          el.find(".{{ s.id }}").{{ s.signal }}(function(ev) {
+            self.{{ s.handler }}(this, ev);
+          });
+  {%- endif %}
+{% endfor %}
+
           // Do your stuff here
 
           return true;
